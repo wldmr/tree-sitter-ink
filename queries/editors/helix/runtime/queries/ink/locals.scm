06bf7f26c76621f8e01d36af423445e174969155
wldmr <nope@wldmr.net>
2026-05-04 17:43:24 +0200

    Fix locals.scm
    Seems like the capture name has changed since I first wrote this.

--- queries/editors/helix/runtime/queries/ink/locals.scm
+++ queries/editors/helix/runtime/queries/ink/locals.scm
@@ -1,9 +1,9 @@
 (knot_block) @local.scope
 (stitch_block) @local.scope
 
-(param (identifier) @local.definition)
-(param (divert (identifier) @local.definition))
-(temp_def name: (identifier) @local.definition)
+(param (identifier) @local.definition.variable.parameter)
+(param (divert (identifier) @local.definition.variable.parameter))
+(temp_def name: (identifier) @local.definition.variable)
 
 (identifier) @local.reference
 (divert (identifier) @local.reference)
